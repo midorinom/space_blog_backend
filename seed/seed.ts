1f19b3e60a255b24f8b7238b36d20d9df25d87ce
Hao Fang <haofang97@gmail.com>
2024-06-18 22:13:30 +0800

    await ConnectDB

--- seed/seed.ts
+++ seed/seed.ts
@@ -5,39 +5,38 @@ import { seedData } from "./seed-data.js";
 
 dotenv.config();
 
-if (!process.env.MONGODB_URI) {
-  console.log("MONGODB_URI is not defined.");
-} else {
-  connectDB(process.env.MONGODB_URI);
-}
-
 const seedDatabase = async () => {
   try {
-    console.log("Seeding the database, please wait...");
+    if (!process.env.MONGODB_URI) {
+      console.log("MONGODB_URI is not defined.");
+    } else {
+      await connectDB(process.env.MONGODB_URI);
+      console.log("Seeding the database, please wait...");
 
-    await Promise.all(
-      seedData.map(async (commentData) => {
-        const existingData = await Comments.findOne({
-          $and: [
-            { article_id: commentData.article_id },
-            { username: commentData.username },
-            { comment: commentData.comment },
-            { date: commentData.date },
-          ],
-        });
+      await Promise.all(
+        seedData.map(async (commentData) => {
+          const existingData = await Comments.findOne({
+            $and: [
+              { article_id: commentData.article_id },
+              { username: commentData.username },
+              { comment: commentData.comment },
+              { date: commentData.date },
+            ],
+          });
 
-        if (!existingData) {
-          // Insert the document if it doesn't exist
-          await Comments.create(commentData);
-        } else {
-          console.log(
-            `Skipping duplicate entry by ${commentData.username} on the article: ${commentData.article_id}.`
-          );
-        }
-      })
-    );
+          if (!existingData) {
+            // Insert the document if it doesn't exist
+            await Comments.create(commentData);
+          } else {
+            console.log(
+              `Skipping duplicate entry by ${commentData.username} on the article: ${commentData.article_id}.`
+            );
+          }
+        })
+      );
 
-    console.log("Done seeding the database.");
+      console.log("Done seeding the database.");
+    }
   } catch (err) {
     console.error("An error occurred when seeding the database:", err);
   } finally {
